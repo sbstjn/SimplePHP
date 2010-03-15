1381e273786c426872a3e61c20bd0a9c3134c8f9
Sebastian Müller <sebastian@hazelcode.com>
2010-03-15 19:47:41 +0100

    Changed File

--- File.class.php
+++ File.class.php
@@ -1,75 +1,95 @@
 <?php
 
 /**
- * File handles file based actions
+ * Handle file actions
  * @package SimplePHP
  * @author Sebastian Müller
  * @version 0.1
- * @link http://github.com/hazelcode/SimplePHPTools
+ * @link http://github.com/hazelcode/SimplePHP
  */
 
-class SiFile {
+class File {
 
-	/**
-	 * Write data to file
-	 * @param string $file
-	 * @param string $content
-	 */
-	static function put($file, $content = '') {
-		$handle = fopen($name,	"w+");
-		fwrite($handle, $data);
-		fclose($handle);
-	}
-	
-	/**
-	 * Write data to file (wrapper for put)
-	 * @param string $file
-	 * @param string $content
-	 */
-	static function write($file, $content = '') {
-		return self::put($file, $content);
-	}
+    /**
+     * construct is not used
+     */
+    public function __construct() {
+    
+    }
+    
+    /**
+     * Check file
+     * @param string $file
+     */
+    static function __checkFile($file) {
+    	if (!file_exists($file))
+    		throw new Exception('File not found: ' . $file);
+    		
+    	return true;
+    }
+    
+    /**
+     * Write data to file
+     * @param string $file
+     * @param string $content
+     */
+    static function put($file, $content = '', $fileOption = 'w+') {
+    	self::__checkFile($file);
+    
+        $handle = fopen($name, $fileOption);
+        fwrite($handle, $data);
+        fclose($handle);
+    }
+    
+    /**
+     * Write data to file (wrapper for put)
+     * @param string $file
+     * @param string $content
+     */
+    static function write($file, $content = '') {
+        return self::put($file, $content);
+    }
 
-	/**
-	 * Write data to file (wrapper for put)
-	 * @param string $file
-	 * @param string $content
-	 */
-	static function save($file, $content = '') {
-		return self::put($file, $content);
-	}
+    /**
+     * Write data to file (wrapper for put)
+     * @param string $file
+     * @param string $content
+     */
+    static function save($file, $content = '') {
+        return self::put($file, $content);
+    }
 
-	/**
-	 * Get content from file
-	 * @param string $file
-	 * @param int $length
-	 * @return string
-	 */
-	static function get($file, $length = 512) {
-		$handle  = fopen ($file, "r");
-		return fread ($handle, $length);
-	}
+    /**
+     * Get content from file
+     * @param string $file
+     * @param int $length
+     * @return string
+     */
+    static function get($file, $length = FILE_SIZE_FULL) {
+    	self::__checkFile($file);
+    	
+        $handle = fopen($file, ($length == FILE_SIZE_FULL ? filesize($file) : $length));
+        return fread ($handle, $length);
+    }
 
-	/**
-	 * Get content from file (wrapper for get)
-	 * @param string $file
-	 * @param int $length
-	 * @return string
-	 */
-	static function load($file, $length = 512) {
-		$handle  = fopen ($file, "r");
-		return fread ($handle, $length);
-	}
+    /**
+     * Get content from file (wrapper for get)
+     * @param string $file
+     * @param int $length
+     * @return string
+     */
+    static function load($file, $length = FILE_SIZE_FULL) {
+        return self::get($file, $length);
+    }
 
-	/**
-	 * Get content from file (wrapper for get)
-	 * @param string $file
-	 * @param int $length
-	 * @return string
-	 */
-	static function getContent($file, $length = 512) {
-		$handle  = fopen ($file, "r");
-		return fread ($handle, $length);
-	}
+    /**
+     * Get content from file (wrapper for get)
+     * @param string $file
+     * @param int $length
+     * @return string
+     */
+    static function getContent($file, $length = FILE_SIZE_FULL) {
+        return self::get($file, $length);
+    }
 
 }
