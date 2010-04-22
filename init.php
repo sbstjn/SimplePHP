9b7750c4315018703200bb96ad5ef76e38371e24
Sebastian Müller <sebastian@hazelcode.com>
2010-04-22 22:24:08 +0200

    Removed SQLJoin, not really simple :|

--- init.php
+++ init.php
@@ -23,6 +23,5 @@ require_once PATH_SIMPLEPHP . 'General.class.php';
 require_once PATH_SIMPLEPHP . 'Array.class.php';
 require_once PATH_SIMPLEPHP . 'File.class.php';
 require_once PATH_SIMPLEPHP . 'SQL.class.php';
-require_once PATH_SIMPLEPHP . 'SQLJoin.class.php';
 
 ?>
